d0a5abb4694278509f3b8e8067b29f63fc9267e6
Bleyle <Bennybleylle09@gmail.com>
2024-09-06 04:34:13 +0300

    Add files via upload
    xmtp chat functionality added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
+import "./polyfills";
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import ReactDOM from "react-dom/client";
+//import InboxPage from "./Page";
 import { MemoryRouter } from "react-router-dom";
 
+import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MemoryRouter>
+     <MemoryRouter>
       <App />
     </MemoryRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
